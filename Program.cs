fc1aeb63ffc2c276f9844c4faefa969ffdfb30a6
Andrea Vacca <E-andrea.vacca@msc.com>
2023-01-31 12:20:40 +0100

    Commit conflict

--- Program.cs
+++ Program.cs
@@ -3,4 +3,5 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Test Message");
 Console.WriteLine("La push baby");
 Console.WriteLine("Hello from terminal");
+Console.WriteLine("Conflict");
 Console.ReadLine();
