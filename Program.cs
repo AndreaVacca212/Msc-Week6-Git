e4e7fcb50f29f52a6b4634476ae8f901e0aa3209
Andrea Vacca <E-andrea.vacca@msc.com>
2023-01-31 12:22:13 +0100

    Commit conflict2

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Test Message");
+Console.WriteLine("Yes this is a conflict mf");
 Console.WriteLine("La push baby");
 Console.WriteLine("Hello from terminal");
 Console.ReadLine();
